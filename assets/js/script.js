9b01666db25acae022dec1c2bc96fc8cc1f760c2
RashadGasimzada <rashad.gasimzade.2000@gmail.com>
2022-02-09 21:00:28 +0400

    Jquery practics

---
+++ assets/js/script.js
@@ -0,0 +1,87 @@
+$(function () {
+
+    //let text = document.querySelector("h1");
+
+    // let ul = $("#list");
+
+    // let list = $("#list li");
+
+    // let btn = $("#button");
+
+    // list.click(function(){
+    //   console.log($(this))
+    // })
+
+    // let count = 1;
+    // btn.click(function(){
+    //     let newLi = $(`<li class="list-group-item">${count}</li>`);
+    //     ul.append(newLi);
+    //     count ++;
+    // })
+
+    // list.click(function () {
+    //   console.log("test")
+    // })
+
+    // $(document).on("click", "#play", function () {
+    //     $(".box").slideToggle(3000);
+    // })
+
+    // $(document).on("click","#button",function(){
+    //     $(".box").css("background-color","red")
+    // })
+
+    // $(document).on("click", "#button", function () {
+    //     $(".box").fadeToggle(2000).css({
+    //         "background-color":"hotpink",
+    //         "width":"300px"
+    //     });
+    // })
+
+    // $(document).on("click", "#stop", function () {
+    //     $(".box").stop();
+    // })
+
+    // $(document).on("click", "#stop", function () {
+    //     $("p").show("slow", function () {
+    //         alert("Salam");
+    //     });
+    // })
+
+    // $(document).on("click", "#play", function () {
+    //     $("p").hide("slow", function () {
+    //         alert("Sagol");
+    //     });
+    // })
+
+    // if($(".test").val() == ""){
+    //     alert("elave edin")
+    // }
+
+    // console.log($(".test").attr("class","bye"))
+
+    // $(document).on("click", "#play", function () {
+    //     $("p").remove()
+    // })
+
+    // $(document).on("click", "#play", function () {
+    //    let endSize = ($(".box").width() * 2 ) + "px" ;
+    //    $(".box").css("width",endSize)
+
+    // })
+
+    //console.log($("#third").parent())
+    //console.log($("#list").children())
+   // console.log($("#list").find("#third"))
+    //console.log($("#list #third").next().text())
+    // console.log($("#list #third").nextAll())
+
+    // console.log($("#list li").first().next().text())
+
+    // console.log($("#list li").eq(2))
+
+    // $(document).on("click",".accordion-header",function(){
+    //     $(this).next().slideToggle();
+    // })
+    
+})
